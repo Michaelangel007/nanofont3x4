265183c75b5d2433cfb944f7d38c78295f30a6ce
michaelangel007 <testcraft@sharedcraft.com>
2015-07-10 17:32:57 -0700

    Bash script to build BMPs

---
+++ build_bmps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p pic
+
+nanofont3x4 -u -0 nanofont3x4.cpp
+mv output_upper3x4.bmp pic/output_nanofont3x4_upper_0.bmp
+
+nanofont3x4 -u -1 nanofont3x4.cpp
+mv output_upper3x4.bmp pic/output_nanofont3x4_upper_1.bmp
+
+nanofont3x4 -u -2 nanofont3x4.cpp
+mv output_upper3x4.bmp pic/output_nanofont3x4_upper_2.bmp
+
+nanofont3x4       nanofont3x4.cpp
+mv output_lower3x4.bmp pic/output_nanofont3x4_lower.bmp
+
+mv nanofont3x.4bmp pic/nanofont3x4.bmp
+
